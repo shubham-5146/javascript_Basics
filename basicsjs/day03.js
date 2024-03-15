26488383feb88ef499660f8565ac90290d149848
Shubham Bairwa <107389006+shubham-5146@users.noreply.github.com>
2024-03-15 17:14:57 +0000

    container config done

---
+++ basicsjs/day03.js
@@ -0,0 +1,21 @@
+const gameName =  ('shubham')
+
+console.log(gameName);
+
+console.log(gameName[2]);
+
+console.log(gameName.length) // it will give the length of the string
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(3));
+
+console.log(gameName.indexOf('m'));
+
+
+console.log(gameName.substring(0,4));
+
+console.log(gameName.slice(-8,1)); // to reverse
+
+console.table([gameName.length, gameName.charAt(4)])
+
