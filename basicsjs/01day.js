bd482619bd962c3fbe269f4bfdb87ddbad81120e
Shubham Bairwa <107389006+shubham-5146@users.noreply.github.com>
2024-03-15 16:59:49 +0000

    container config done

---
+++ basicsjs/01day.js
@@ -0,0 +1,18 @@
+const ApplicationId = 6344 //we cant edit once defined
+
+let Name =  "Shubham Bairwa" //editable
+
+var Email = "abc@gmail.com" //editable
+
+
+console.log(ApplicationId);
+
+console.log(Name);
+
+console.log(Email);
+
+
+
+
+
+
